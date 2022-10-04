93fbb59adbf165f8048b2fdd869a383a5244e759
Adri96MH <49154252+Adri96MH@users.noreply.github.com>
2022-10-04 12:05:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-5-angular-9-start-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-5-angular-9-start-template-zs7tjz)
---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,17 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Tab1Page } from './tab1.page';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }])
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
